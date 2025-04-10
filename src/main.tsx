e93807c9a9cc7593cbccdbc5a95db8842056765a
Clark Winters <github@clarkwinters.com>
2025-04-10 11:49:52 -0500

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import * as Sentry from "@sentry/react"
+import "./index.css"
+import App from "./App.tsx"
+
+Sentry.init({
+  debug: true,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllInputs: false,
+      maskAllText: false,
+      blockAllMedia: false,
+      networkDetailAllowUrls: [
+        window.location.origin,
+        import.meta.env.VITE_REGEN_ENDPOINT,
+      ],
+      _experiments: {
+        traceInternals: true,
+        captureExceptions: true,
+      },
+    }),
+    Sentry.replayCanvasIntegration(),
+  ],
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+})
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
